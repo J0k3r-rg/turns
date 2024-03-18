9714d8f3b6505de4b1fe1371016ea03eb675145e
j0k3r <pdasriogallegos@gmail.com>
2024-03-18 09:58:33 -0300

    init project

---
+++ src/main/java/com/j0k3r/turns/TurnsApplication.java
@@ -0,0 +1,13 @@
+package com.j0k3r.turns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j0k3r/turns/TurnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j0k3r.turns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
